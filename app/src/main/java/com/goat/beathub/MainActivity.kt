08e19edb1e1abec1738e67556b310fb270d6b8d7
AzureAD\AdarshJaganPal <adarsh.pal@skosystems.com>
2025-01-14 16:00:16 +0530

    add login fragment

---
+++ app/src/main/java/com/goat/beathub/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.goat.beathub
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import com.goat.beathub.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the NavController from the NavHostFragment
+    }
+
+    // Handle the up button (back press) in the ActionBar
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+}
